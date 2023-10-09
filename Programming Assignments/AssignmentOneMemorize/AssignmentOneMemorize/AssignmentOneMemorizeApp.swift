2697e085cecff749abfca87a2ac1717fa02105c4
AdrianCAG <75263760+AdrianCAG@users.noreply.github.com>
2023-10-09 08:08:22 +0100

    Assignment-1 files

---
+++ Programming Assignments/AssignmentOneMemorize/AssignmentOneMemorize/AssignmentOneMemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  AssignmentOneMemorizeApp.swift
+//  AssignmentOneMemorize
+//
+//  Created by Adrian on 08/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct AssignmentOneMemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
